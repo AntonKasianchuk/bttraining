75019eebc2f5627f1e1920d890f1fd00d4019780
Anton_Kasianchuk <Anton_Kasianchuk@epam.com>
2015-06-10 14:37:15 +0300

    Changed urlpattern to "/pay"

--- src/com/bttraining/FrontServlet.java
+++ src/com/bttraining/FrontServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class FrontServlet
  */
-@WebServlet(description = "Front controller for bttraining project", urlPatterns = { "/*" })
+@WebServlet(description = "Front controller for bttraining project", urlPatterns = { "/pay" })
 public class FrontServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
