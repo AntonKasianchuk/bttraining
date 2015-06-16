9b17b16be96f11cffaf199579c8098623c055bd2
Anton_Kasianchuk <Anton_Kasianchuk@epam.com>
2015-06-16 16:59:16 +0300

    Resolved merge conflict

--- src/com/bttraining/service/CustomerService.java
+++ src/com/bttraining/service/CustomerService.java
@@ -4,10 +4,15 @@ import java.util.Set;
 
 import com.braintreegateway.Customer;
 import com.braintreegateway.ResourceCollection;
+import com.bttraining.web.dto.CustomerDTO;
 
 public interface CustomerService {
-
 	ResourceCollection<Customer> getAllCustomers();
 
 	Set<String> getCustomerIds();
+
+	ResourceCollection<Customer> getCustomerById(String cutomerId);
+	
+	CustomerDTO getCustomerDTOById(String cutomerId);
+
 }
