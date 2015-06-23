73ed2d1b06f491aec65f2dc8d0230235968cf263
Oleg_Bovt <Oleg_Bovt@epam.com>
2015-06-23 12:37:34 +0300

    Organize import.

--- src/com/bttraining/service/CustomerService.java
+++ src/com/bttraining/service/CustomerService.java
@@ -6,7 +6,6 @@ import com.braintreegateway.Customer;
 import com.braintreegateway.CustomerRequest;
 import com.braintreegateway.ResourceCollection;
 import com.braintreegateway.Result;
-import com.bttraining.dao.CustomerDao;
 
 public interface CustomerService {
 	Result<Customer> createCustomer(CustomerRequest customerRequest);
